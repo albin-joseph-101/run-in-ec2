ab11be46fb616563692a14e35ab98c89b1450f8d
Albin Joseph <albin.joseph@batterysmart.in>
2022-08-05 18:35:56 +0530

    delete topic

--- producer.js
+++ producer.js
@@ -9,54 +9,57 @@ const { Kafka } = require('kafkajs');
             "b-3.iotstream.rnnl6v.c6.kafka.us-east-2.amazonaws.com:9094",
         ],
         ssl: true,
-        retry: 30
     });
 
     const admin = kafka.admin();
     await admin.connect();
 
-    const cluster = await admin.describeCluster();
-    const metadata = await admin.fetchTopicMetadata(["test-topic-123", "test-topic", "test-topic-1"])
-    console.log({ cluster: JSON.stringify(cluster), metadata: JSON.stringify(metadata) });
+    // const cluster = await admin.describeCluster();
+    // const metadata = await admin.fetchTopicMetadata(["test-topic-123", "test-topic", "test-topic-1"])
+    // console.log({ cluster: JSON.stringify(cluster), metadata: JSON.stringify(metadata) });
 
     try {
-        const topicjs = await admin.createTopics({
-            waitForLeaders: true,
-            topics: [{ topic: "test-topic-123", numPartitions: 3, replicationFactor: 3 }],
+        const topicjs = await admin.deleteTopics({
+            topics: ["test-topic-123"],
         })
-        console.log({ topicjs });
+        // await admin.deleteTopics({
+        //     waitForLeaders: true,
+        //     topics: [{ topic: "test-topic-123", numPartitions: 3, replicationFactor: 3 }],
+        // })
+        const metadata = await admin.fetchTopicMetadata()
+        console.log({ topicjs, metadata: JSON.stringify(metadata) });
     } catch (error) {
         console.log(error);
     }
 
 
 
-    const producer = kafka.producer({
-        allowAutoTopicCreation: true,
-        retry: 30
-    })
+    // const producer = kafka.producer({
+    //     allowAutoTopicCreation: true,
+    //     retry: 30
+    // })
 
-    await producer.connect()
+    // await producer.connect()
 
-    const res = await producer.send({
-        topic: 'test-topic-123',
-        messages: [
-            { value: 'Hello KafkaJS user!1' },
-            { value: 'Hello KafkaJS user!2' },
-            { value: 'Hello KafkaJS user!3' },
-            { value: 'Hello KafkaJS user!4' },
-            { value: 'Hello KafkaJS user!5' },
-            { value: 'Hello KafkaJS user!6' },
-            { value: 'Hello KafkaJS user!1' },
-            { value: 'Hello KafkaJS user!2' },
-            { value: 'Hello KafkaJS user!3' },
-            { value: 'Hello KafkaJS user!4' },
-            { value: 'Hello KafkaJS user!5' },
-            { value: 'Hello KafkaJS user!6' },
-        ],
-    })
-    console.log({res});
-    await producer.disconnect();
+    // const res = await producer.send({
+    //     topic: 'test-topic-123',
+    //     messages: [
+    //         { value: 'Hello KafkaJS user!1' },
+    //         { value: 'Hello KafkaJS user!2' },
+    //         { value: 'Hello KafkaJS user!3' },
+    //         { value: 'Hello KafkaJS user!4' },
+    //         { value: 'Hello KafkaJS user!5' },
+    //         { value: 'Hello KafkaJS user!6' },
+    //         { value: 'Hello KafkaJS user!1' },
+    //         { value: 'Hello KafkaJS user!2' },
+    //         { value: 'Hello KafkaJS user!3' },
+    //         { value: 'Hello KafkaJS user!4' },
+    //         { value: 'Hello KafkaJS user!5' },
+    //         { value: 'Hello KafkaJS user!6' },
+    //     ],
+    // })
+    // console.log({res});
+    // await producer.disconnect();
     await admin.disconnect();
     console.log("done");
 
