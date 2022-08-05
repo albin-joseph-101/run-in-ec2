badecf0c78041e9eceb3e0884333d7ab302dfb06
Albin Joseph <albin.joseph@batterysmart.in>
2022-08-05 18:18:57 +0530

    dfsa

--- consumer.js
+++ consumer.js
@@ -4,16 +4,16 @@ const { Kafka } = require('kafkajs');
     const kafka = new Kafka({
         clientId: 'iot-msk-producer',
         brokers: [
-            "b-2.iotstream.rnnl6v.c6.kafka.us-east-2.amazonaws.com:9098",
-            "b-1.iotstream.rnnl6v.c6.kafka.us-east-2.amazonaws.com:9098",
-            "b-3.iotstream.rnnl6v.c6.kafka.us-east-2.amazonaws.com:9098",
+            "b-2.iotstream.rnnl6v.c6.kafka.us-east-2.amazonaws.com:9094",
+            "b-1.iotstream.rnnl6v.c6.kafka.us-east-2.amazonaws.com:9094",
+            "b-3.iotstream.rnnl6v.c6.kafka.us-east-2.amazonaws.com:9094",
         ],
     });
 
     const consumer = kafka.consumer({ groupId: 'test-group' })
 
     await consumer.connect()
-    await consumer.subscribe({ topic: 'test-topic', fromBeginning: true })
+    await consumer.subscribe({ topic: 'test-topic-123', fromBeginning: true })
     await consumer.run({
         eachMessage: async ({ topic, partition, message }) => {
             console.log({
